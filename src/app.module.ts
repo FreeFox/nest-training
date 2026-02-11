63f11188e4d2be41823237a2aecbbfcae0a45976
Roman Lysiuk <roma.freefox@gmail.com>
2026-02-12 01:04:37 +0200

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfilesModule } from './profiles/profiles.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProfilesModule],
+  imports: [
+    ProfilesModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
