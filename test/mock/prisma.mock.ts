8dc06efcdcc70ae8c49adeb185a47919e7ac8f9f
Roman Lysiuk <roma.freefox@gmail.com>
2026-02-12 19:33:48 +0200

    add methods

--- test/mock/prisma.mock.ts
+++ test/mock/prisma.mock.ts
@@ -2,8 +2,11 @@ export const mockPrismaService = {
   profile: {
     findUnique: jest.fn(),
     findMany: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
     create: jest.fn(),
     update: jest.fn(),
     delete: jest.fn(),
+    remove: jest.fn(),
   },
 };
