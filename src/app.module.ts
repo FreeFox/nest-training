76407b0ff99bb83eafcd24842cbc754f73043a9e
Roman Lysiuk <roma.freefox@gmail.com>
2026-02-13 18:07:08 +0200

    auth module + service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfilesModule } from './profiles/profiles.module';
-import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ProfilesModule,
-    AuthModule,
     UsersModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
