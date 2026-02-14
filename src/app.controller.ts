7da835ccbc6b6b3f802a0dc433c1c2a5293072a6
Roman Lysiuk <roma.freefox@gmail.com>
2026-02-14 03:15:58 +0200

    jwt integration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,14 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
-  }
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/logout')
-  async logout(@Request() req) {
-    return req.logout();
+    return this.authService.login(req.user);
   }
 }
